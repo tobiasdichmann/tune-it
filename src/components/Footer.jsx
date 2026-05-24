27a943a6616aca459157a8628bf2512eef1987da
Tobias <tobias.dichmanngmail.com>
2026-05-24 12:31:53 +0200

    Updated the footer design
    I have completely changed the overall design of the footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,37 +5,58 @@ import "../styles/footer.scss";
 import { FaInstagram, FaFacebookF } from "react-icons/fa";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <footer>
-      <div className="contacts">
-        <p>
-          Skriv til os, hvis I har spørgsmål: <br />
-          <a href="mailto:susan@tune-it.dk">susan@tune-it.dk</a>
-        </p>
+    <footer className="site-footer">
+      <div className="footer-content">
+        <div className="footer-brand">
+          <h2 className="brand-logo">Tune It</h2>
+          <p className="brand-tagline">Vokalensemble fra Randers</p>
+        </div>
+
+        <div className="footer-info">
+          <div className="contacts">
+            <span className="footer-label">Kontakt</span>
+            <p>
+              Skriv til os, hvis I har spørgsmål:<br />
+              <a href="mailto:susan@tune-it.dk" className="email-link">
+                susan@tune-it.dk
+              </a>
+            </p>
+          </div>
+
+          <div className="socials">
+            <span className="footer-label">Find os her</span>
+            <div className="socials-icons">
+              <a
+                href="https://www.instagram.com/tune_it_randers/?hl=da"
+                title="@tune_it_randers"
+                target="_blank"
+                rel="noreferrer"
+                className="social-icon-btn"
+              >
+                <FaInstagram />
+              </a>
+
+              <a
+                href="https://www.facebook.com/tuneit?locale=da_DK"
+                title="Tune It"
+                target="_blank"
+                rel="noreferrer"
+                className="social-icon-btn"
+              >
+                <FaFacebookF />
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <div className="socials">
-        <p>Find os her:</p>
-
-        <div className="socials-icons">
-          <a
-            href="https://www.instagram.com/tune_it_randers/?hl=da"
-            title="@tune_it_randers"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaInstagram />
-          </a>
-
-          <a
-            href="https://www.facebook.com/tuneit?locale=da_DK"
-            title="Tune It"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaFacebookF />
-          </a>
-        </div>
+      <div className="footer-bottom">
+        <p className="copyright">
+          &copy; {currentYear} Tune It. Alle rettigheder forbeholdes.
+        </p>
       </div>
     </footer>
   );
